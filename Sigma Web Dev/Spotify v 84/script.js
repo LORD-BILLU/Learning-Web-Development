8ebfd56a084b39a313dbbc2da45bb464bea5dabe
LORD-BILLU <ankitlap6213@gmail.com>
2026-05-27 22:44:41 +0530

    backend

--- Sigma Web Dev/Spotify v 84/script.js
+++ Sigma Web Dev/Spotify v 84/script.js
@@ -11,7 +11,10 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("songs")[1]);
+            let ele = element.href.split("songs")[1].replaceAll("%20"," ");
+            let final = ele.replaceAll("%5C"," ");
+            final = final.trim();
+            songs.push(final);
         }
     }
 
@@ -20,18 +23,17 @@ async function getSongs(){
 }
 
 let current_sng = new Audio();
+let curr_idx = -1;
 
 async function main(){
     let songs = await getSongs();
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs){
-        let ele = song.replaceAll("%20"," ");
-        let final = ele.replaceAll("%5C"," ");
         songUL.innerHTML = songUL.innerHTML + `<li>
                             <img src="music.svg" class="invert" alt="music-logo">
                             <div class="info">
-                                <div class="name">${final}</div>
+                                <div class="name">${song}</div>
                                 <div>LORD BILLU</div>
                             </div>
                             <img src="play.svg" class="invert" alt="play-pause">
@@ -41,13 +43,65 @@ async function main(){
     console.log(songs);
     
     let song1 = Array.from(document.querySelector(".songList").getElementsByTagName("ul")[0].getElementsByTagName("li"));
-    song1.forEach((element) => {
+    song1.forEach((element,index) => {
         let n1 = element.querySelector(".name").innerText;
         element.addEventListener('click',()=>{
             let path = `songs/${n1}`;
-            let audio = new Audio(path);
-            audio.play();
+
+            current_sng.pause();
+            current_sng.currentTime = 0;
+
+            current_sng.src = path;
+            curr_idx = index;
+            current_sng.play();
         });
+    });
+
+    const prev = document.getElementById("prev");
+    const play = document.getElementById("play");
+    const next = document.getElementById("next");
+
+    prev.addEventListener("click",()=>{
+        if(curr_idx===0){
+            curr_idx=9;
+        }
+        else{
+            curr_idx--;
+        }
+
+        let path = `songs/${songs[curr_idx]}`;
+
+        current_sng.pause();
+        current_sng.currentTime = 0;
+
+        current_sng.src = path;
+            
+        current_sng.play();
+
+    });
+
+    play.addEventListener("click",()=>{
+        
+        
+    });
+
+    next.addEventListener("click",()=>{
+        if(curr_idx===9){
+            curr_idx=0;
+        }
+        else{
+            curr_idx++;
+        }
+
+        let path = `songs/${songs[curr_idx]}`;
+
+        current_sng.pause();
+        current_sng.currentTime = 0;
+
+        current_sng.src = path;
+            
+        current_sng.play();
+
     });
 }
 
---
+++ Sigma Web Dev/v 85/script.js
@@ -0,0 +1,3 @@
+console.log("Intro to Backend");
+
+
