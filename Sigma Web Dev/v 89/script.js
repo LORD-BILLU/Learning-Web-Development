92526f134bd0c06ec3f7d3b73817122b3117cbe5
influencer-Billu <ankit97755@gmail.com>
2026-05-28 22:23:45 +0530

    no err

--- Sigma Web Dev/v 89/script.js
+++ Sigma Web Dev/v 89/script.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! POST request')
+  res.send('Hello World! POST request yeah yeah')
 })
 
 app.listen(port, () => {
@@ -12,3 +12,7 @@ app.listen(port, () => {
 
 // i have no idea what the fuck is going on in this video
 // idk man i am gonna go with the flow 
+
+// i kinda understand post man now and i think post man i kinda good really learn it
+//Express ROuter is kinda sus gotta do it again
+// to prevent cluttering use router still gotta learn it though
